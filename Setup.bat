72faf158e285a4bb0963c2e9c8384b14d49fadca
Artificer <rtificr@gmail.com>
2024-12-24 12:43:02 -0500

    Modified Setup.bat to better inform the user.

--- Setup.bat
+++ Setup.bat
@@ -1,10 +1,9 @@
 @echo off
-echo "DEBIAN ONLY (if you're an Arch user, figure it out yourself)"
 
 :: Check if git is installed, if not, terminate the program
 git --version > nul 2>&1
 if %errorlevel% neq 0 (
-    echo Git is not installed. Please install Git from https://git-scm.com/.
+    echo Git is not installed. Please install Git from https://git-scm.com/, or manually download and install the build.
     exit /b 1
 ) else (
     echo Git is already installed.
